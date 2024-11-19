3d01671405bb09a0c661a5360e2c35c4c07db6e6
Null Entity <entitynull347@gmail.com>
2024-11-19 23:39:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
-// Script PannOfficiaL V2
+// Script Echo V2
 
 require("./database/module")
 
-//GLOBAL PAYMENT
-global.storename = "PannOfficiaLDeveloper"
+// GLOBAL PAYMENT
+global.storename = "NullEntity"
 global.dana = "0831"
 global.qris = "-"
 
-
 // GLOBAL SETTING
-global.owner = "6283896168449"
-global.namabot = "💧⃟𒆜𝐏𝐚𝐧𝐧𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥𝟒𝟒𝟒⃟╳🐦‍🔥"
-global.nomorbot = "6283845850116"
-global.namaCreator = "💧⃟𒆜𝐏𝐚𝐧𝐧𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥𝟒𝟒𝟒⃟╳🐦‍🔥"
-global.linkyt = "https://www.youtube.com/@PannOfficialDev" // btw lom ada cik yt nya, insyaallah kpn" tak upload 😋😋
+global.owner = "Null Entity"
+global.namabot = "Echo"
+global.nomorbot = "263780858655"
+global.namaCreator = "Null Entity"
+global.linkyt = "https://www.youtube.com/@PannOfficialDev" // YouTube link remains unchanged
 global.autoJoin = false
 global.antilink = false
 global.versisc = '2.0'
@@ -21,17 +20,13 @@ global.versisc = '2.0'
 // DELAY JPM
 global.delayjpm = 5500
 
-
-
-//GLOBAL THUMB
-
+// GLOBAL THUMB
 global.codeInvite = ""
 global.imageurl = 'https://k.top4top.io/p_3241oy2j10.jpg'
 global.isLink = "https://whatsapp.com/channel/0029VarGepU1dAw1U76h3S1k"
 global.packname = "Bugs"
-global.author = "PannOfficiaL"
+global.author = "Null Entity"
 global.jumlah = "5"
-
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
